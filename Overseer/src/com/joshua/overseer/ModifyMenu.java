514fd75c98dd94ab02ceaa0204e5fed91db4be9a
FrostyGelato <zirconcrystal@outlook.com>
2020-09-02 00:47:15 -0700

    added components to ModifyMenu

--- Overseer/src/com/joshua/overseer/ModifyMenu.java
+++ Overseer/src/com/joshua/overseer/ModifyMenu.java
@@ -9,17 +9,47 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JSpinner;
 
 public class ModifyMenu extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
+	private JTextField textField;
 
 	public ModifyMenu() {
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		{
+			JLabel lblNewLabel = new JLabel("Name:");
+			lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
+			lblNewLabel.setBounds(12, 13, 56, 16);
+			contentPanel.add(lblNewLabel);
+		}
+		
+		textField = new JTextField();
+		textField.setBounds(70, 8, 197, 28);
+		contentPanel.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Time Required:");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNewLabel_1.setBounds(12, 59, 115, 20);
+		contentPanel.add(lblNewLabel_1);
+		
+		JSpinner spinner = new JSpinner();
+		spinner.setBounds(139, 56, 76, 28);
+		contentPanel.add(spinner);
+		
+		JLabel lblNewLabel_2 = new JLabel("Deadline:");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNewLabel_2.setBounds(12, 109, 76, 16);
+		contentPanel.add(lblNewLabel_2);
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
@@ -47,5 +77,4 @@ public class ModifyMenu extends JDialog {
 			}
 		}
 	}
-
 }

0a8af0b1bcf300b923e7525ebbc3e89fe2f5884f
FrostyGelato <zirconcrystal@outlook.com>
2020-10-14 23:21:22 +0800

    partially fixed loading schedule bug

--- Overseer/src/com/joshua/overseer/AddMenu.java
+++ Overseer/src/com/joshua/overseer/AddMenu.java
@@ -109,7 +109,7 @@ public class AddMenu extends JDialog {
 						// localTime is automatically set to midnight if not chosen
 						taskManager.addTask(taskName.getText(), length, deadlinePicker.getDateTimePermissive());
 						// refreshes schedule in parent JFrame
-						parentJFrame.checkAndRefreshSchedule();
+						parentJFrame.checkAndLoadSchedule();
 						parentJFrame.checkAndLoadTimer();
 						parentJFrame.refresh();
 						dispose();
